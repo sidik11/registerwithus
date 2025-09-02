63e19edf1b7ec007be91390f62466e39b49be4ac
Ritik Rosan Sahoo <ritiksahoors@gmail.com>
2025-09-02 14:16:57 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,2 @@
-export const API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:3001"
-    : "https://testlink.registerwithus.in/";
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+console.log("Loaded API_BASE_URL =", API_BASE_URL);
