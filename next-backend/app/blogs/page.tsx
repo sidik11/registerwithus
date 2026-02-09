2997d89d794a06f85d35b6c86edb74faa5555449
Sidik <sidik11@gmail.com>
2026-02-09 10:37:20 +0530

    Sync local changes after stash pop

--- next-backend/app/blogs/page.tsx
+++ next-backend/app/blogs/page.tsx
@@ -6,14 +6,29 @@ export const metadata = {
   },
 };
 
-export default async function BlogPage() {
-  const blogs = await fetch("https://api.yoursite.com/blogs", {
+type Blog = {
+  slug: string;
+  title: string;
+};
+
+async function getBlogs(): Promise<Blog[]> {
+  const res = await fetch("https://api.yoursite.com/blogs", {
     cache: "no-store",
-  }).then(r => r.json());
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch blogs");
+  }
+
+  return res.json();
+}
+
+export default async function BlogPage() {
+  const blogs = await getBlogs();
 
   return (
     <main>
-      {blogs.map(b => (
+      {blogs.map((b) => (
         <a key={b.slug} href={`/blogs/${b.slug}`}>
           {b.title}
         </a>
